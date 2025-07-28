a9a08e960ae49c102d7700141d448feb38c86c8b
jitendra Arya <aryajitendra038@gmail.com>
2025-07-28 22:00:15 +0530

    audio record update

---
+++ components/AllAudio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllAudio = () => {
+  return (
+    <div className='w-1/3 bg-orange-100 '>
+        
+    </div>
+  )
+}
+
+export default AllAudio
--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import React ,{ useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css'
-// import AudioRecorder from "../components/AudioRecorder"
+import AudioRecorder from "../components/AudioRecorder"
 import FileUpload from '../components/fileUpload'
-import { transcribeAudio } from '../utils/transcribeAudio';
+
 
 import TranscriptDisplay from '../components/TranscriptDisplay';
+import AllAudio from '../components/AllAudio';
 
 function App() {
   const [transcript, setTranscript] = useState('');
 
   const handleAudioBlob = async (blob) => {
-    
-      const formData = new FormData();
-      formData.append('audio', blob, 'recorded_audio.webm');
 
-      const response = await axios.post("http://localhost:5000/api/audio/upload", formData);
+    const formData = new FormData();
+    formData.append('audio', blob, 'recorded_audio.webm');
 
-      setTranscript(response.data.transcript);
+    const response = await axios.post("http://localhost:5000/api/audio/upload", formData);
 
- 
+
+    const audioId = response.data.audioId;
+    // console.log(audioId);
+
+    const res = await axios.post("http://localhost:5000/api/audio/transcribe", { audioId });
+
+    //console.log(res);
+
+
+
+
+    setTranscript(res.data.transcript.transcript);
+    //  console.log(response.data);
+
   };
 
   return (
     <>
-      <div className="p-6 max-w-xl mx-auto space-y-6 bg-pink-100 w-full">
-        <h1 className="text-2xl font-bold text-center"> Speech to Text App</h1>
-        <FileUpload onTranscript={setTranscript} />
-        {/* <AudioRecorder onAudioReady={handleAudioBlob} /> */}
-       <TranscriptDisplay transcript={transcript}/>
-      </div>
+      <div className=' h-screen  w-screen m-auto flex item-center justify-center'>
+        <div className="p-6 w-2/3 bg-pink-100 ">
+           <h1 className="text-black text-3xl font-extrabold shadow-white-4 text-center underline "> Speech to Text App</h1>
+          <FileUpload onTranscript={setTranscript} />
+          <AudioRecorder onAudioReady={handleAudioBlob} />
+          <TranscriptDisplay transcript={transcript} />
+          <AllAudio />
+        </div>
 
+      </div>
     </>
   )
 }
