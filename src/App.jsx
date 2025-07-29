aa2b6e3b4e477729380df3b7f203e7f00efea3f7
jitendra Arya <aryajitendra038@gmail.com>
2025-07-29 21:37:24 +0530

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import AudioRecorder from '../components/audioRecorder';
+import AudioRecorder from '../components/AudioRecorder';
 import FileUpload from '../components/fileUpload';
 import TranscriptDisplay from '../components/TranscriptDisplay';
 
